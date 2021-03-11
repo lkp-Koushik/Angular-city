a177a766433dbcea4533234cf76a430edb352f3a
lkp-Koushik <lkp.koushik@gmail.com>
2021-03-11 19:51:33 +0530

    4 files modified, 17 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,35 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
-
 import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
+import { TracksComponent } from './tracks/tracks.component';
+import { AppRoutingModule } from './app-routing.module';
+import { TracksSummaryComponent } from './tracks-summary/tracks-summary.component';
+import { LeftNavComponent } from './left-nav/left-nav.component';
+import { TopNavComponent } from './top-nav/top-nav.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatIconModule} from '@angular/material/icon';
+import { MatMenuModule} from '@angular/material/menu';
+import { ChartsModule } from 'ng2-charts';
+import { DoughnutChartComponent } from './doughnut-chart/doughnut-chart.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
-  bootstrap:    [ AppComponent ]
+  declarations: [
+    AppComponent,
+    TracksComponent,
+    TracksSummaryComponent,
+    LeftNavComponent,
+    TopNavComponent,
+    DoughnutChartComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatIconModule,
+    MatMenuModule,
+    ChartsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
